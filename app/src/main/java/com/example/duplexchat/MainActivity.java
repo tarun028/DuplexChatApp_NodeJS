828050ea30537a77e861cbbd0f27e3a0f6c2cc98
tarun028 <tarunsrivastavakvs@gmail.com>
2023-10-06 04:58:41 +0530

    yo

--- app/src/main/java/com/example/duplexchat/MainActivity.java
+++ app/src/main/java/com/example/duplexchat/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.duplexchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.editText);
+
+        findViewById(R.id.enterBtn).setOnClickListener(view -> {});
     }
 }
