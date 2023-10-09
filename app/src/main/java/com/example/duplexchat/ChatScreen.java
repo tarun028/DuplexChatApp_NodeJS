4bbf0fbbcec73f9b50f3168a7ca7afe35599d38a
tarun028 <tarunsrivastavakvs@gmail.com>
2023-10-09 14:48:26 +0530

    it works

--- app/src/main/java/com/example/duplexchat/ChatScreen.java
+++ app/src/main/java/com/example/duplexchat/ChatScreen.java
@@ -1,14 +1,183 @@
 package com.example.duplexchat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Base64;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class ChatScreen extends AppCompatActivity {
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+
+public class ChatScreen extends AppCompatActivity implements TextWatcher {
+
+    private String name;
+    private WebSocket webSocket;
+    private String SERVER_PATH = "ws://echo.websocket.org";
+    private EditText messageEdit;
+    private View sendBtn, pickImgBtn;
+    private RecyclerView recyclerView;
+    private int IMAGE_REQUEST_ID = 1;
+    private MessageAdapter messageAdapter;
+
+    // Declare an ActivityResultLauncher for picking an image
+    private final ActivityResultLauncher<Intent> pickImageLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    // The user picked an image
+                    Uri imageUri = result.getData().getData();
+                    sendImage(imageUri);
+                } else {
+                    // The user cancelled the pick image activity
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_screen);
+
+        name = getIntent().getStringExtra("name");
+        initiateSocketConnection();
+        initializeView();
+
+    }
+
+    private void initiateSocketConnection() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(SERVER_PATH).build();
+        webSocket = client.newWebSocket(request, new SocketListener());
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        String string = s.toString().trim();
+
+        if (string.isEmpty()) {
+            resetMessageEdit();
+        } else {
+            sendBtn.setVisibility(View.VISIBLE);
+            pickImgBtn.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void resetMessageEdit() {
+        messageEdit.removeTextChangedListener(this);
+        messageEdit.setText("");
+        sendBtn.setVisibility(View.INVISIBLE);
+        pickImgBtn.setVisibility(View.VISIBLE);
+        messageEdit.addTextChangedListener(this);
+    }
+
+    private class SocketListener extends WebSocketListener {
+        // ... (No changes in this part)
+    }
+
+    private void initializeView() {
+        messageEdit = findViewById(R.id.messageEdit);
+        sendBtn = findViewById(R.id.sendBtn);
+        pickImgBtn = findViewById(R.id.pickImgBtn);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        messageAdapter = new MessageAdapter(getLayoutInflater());
+        recyclerView.setAdapter(messageAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        messageEdit.addTextChangedListener(this);
+
+        sendBtn.setOnClickListener(v -> {
+            // ... (No changes in this part)
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("name", name);
+                jsonObject.put("message", messageEdit.getText().toString());
+
+                webSocket.send(jsonObject.toString());
+
+                jsonObject.put("isSent", true);
+                messageAdapter.addItem(jsonObject);
+
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount() - 1);
+
+                resetMessageEdit();
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        pickImgBtn.setOnClickListener(v -> {
+            // Create an intent to pick an image
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+
+
+            // Launch the image picker using the Activity Result API
+            pickImageLauncher.launch(intent);
+        });
+    }
+
+    private void sendImage(Uri imageUri) {
+        try {
+            InputStream is = getContentResolver().openInputStream(imageUri);
+            Bitmap image = BitmapFactory.decodeStream(is);
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            image.compress(Bitmap.CompressFormat.JPEG, 50, outputStream);
+
+            String base64String = Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
+
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("name", name);
+                jsonObject.put("image", base64String);
+
+                webSocket.send(jsonObject.toString());
+
+                jsonObject.put("isSent", true);
+                messageAdapter.addItem(jsonObject);
+
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount() - 1);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/duplexchat/MainActivity.java
+++ app/src/main/java/com/example/duplexchat/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.duplexchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
 
         EditText editText = findViewById(R.id.editText);
 
-        findViewById(R.id.enterBtn).setOnClickListener(view -> {});
+        findViewById(R.id.enterBtn).setOnClickListener(view -> {
+            Intent intent = new Intent(this,ChatScreen.class);
+            intent.putExtra("name",editText.getText().toString());
+            startActivity(intent);
+        });
     }
 }
